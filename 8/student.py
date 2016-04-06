d3721663ade5446f94322af184499bf66785213b
michael <misha.baturin@gmail.com>
2016-04-06 23:54:18 +0300

    python exercises

--- 8/student.py
+++ 8/student.py
@@ -1,56 +1,48 @@
 class Student:
-#Google: python attributes
-	exam, result, passed = int(), int(), bool()
 
 	def __init__(self, name, conf):
 		self.name = name
 		self.conf = conf
-		global labs
-		labs = [0] * conf.get('lab_num')
-
+		self.labs = [0] * conf.get('lab_num')
+		self.exam = int()
 
 	def make_lab(self, m, n = None):
-		global labs
-		if m > 7:
-			m = 7
+		if m > self.conf.get('lab_max'):
+			m = self.conf.get('lab_max')
 		if n == None:
-			for i in labs:
-				if labs[i] == 0:
-					labs[i] = m
-					break
-				else:
-					continue
-					print('Continuing')
-		elif n <= len(labs):
-			labs[n] = m
+			self.labs[next(x for x, i in enumerate(self.labs) if i == 0)] = m
+		else:
+			self.labs[n] = m
 
 	def make_exam(self, m):
-		global exam
-		if m in range(0,30) and m > 0:
-			exam = m
-		elif m > 30:
-			exam = 30
+		if m > self.conf.get('exam_max'):
+			m = self.conf.get('exam_max')
+		self.exam = m
 
 	def is_certified(self):
-		global labs, exam
-		result = sum(labs) + exam
-		if result/100 >= self.conf.get('k'):
-			passed = True
+		if (sum(self.labs) + self.exam)/100 >= self.conf.get('k'):
+			return ( (sum(self.labs) + self.exam), True )
 		else:
-			passed = False
-		return (result, passed)
+			return ( (sum(self.labs) + self.exam), False )
 
+#Testing:
 
 conf = {'exam_max' : 30, 'lab_max' : 7, 'lab_num' : 10, 'k' : 0.61 }
 oleg = Student('Oleg', conf)
 oleg.make_lab(1)
+print(oleg.labs)
 oleg.make_lab(8, 0)
+print(oleg.labs)
 oleg.make_lab(1)
+print(oleg.labs)
 oleg.make_lab(10,7)
+print(oleg.labs)
 oleg.make_lab(4,1)
+print(oleg.labs)
 oleg.make_lab(5)
+print(oleg.labs)
 oleg.make_lab(6.5)
-print(labs)
+print(oleg.labs)
 oleg.make_exam(32)
 print(oleg.is_certified())
 oleg.make_lab(7,1)
